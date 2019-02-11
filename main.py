9c9feff1b2b1586b28ad305be2f45c7bd4258cba
HorseBigboat <474528538@qq.com>
2019-02-11 18:54:33 +0800

    jinja2

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
-
-
-@app.route('/request')
-def req():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Your browser is %s</h1>' % user_agent
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
